271f74bddbb381a45432194f8ef5b331dc063400
EgorZareckii <zed0817@gmail.com>
2025-02-20 20:18:29 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int substraction(int a, int b);
 int main(){
     int a = 10;
     int b = 5;
-    std::cout<<"Result:"<<addition(a,b)<<std::endl;
-    std::cout<<"Result:"<<substraction(a,b)<<std::endl;
+    std::cout<<"Result of addition of 2 numbers:"<<addition(a,b)<<std::endl;
+    std::cout<<"Result of substraction of 2 numbers:"<<substraction(a,b)<<std::endl;
     return 0;
 }
